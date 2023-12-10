0e038cb0e633d50c3ed4db6a8b18879010ec5b87
Aurorien <starlightnoise@gmail.com>
2023-12-10 17:33:02 +0100

    Add try another react router structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,47 @@
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./views/Home";
 import Dockyard from "./views/Dockyard";
 
-import { createHashRouter, Outlet, RouterProvider } from "react-router-dom";
-
-function Root() {
+function App() {
   return (
-    <>
-      <main>
-        <Outlet />
-      </main>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dockyard" element={<Dockyard />} />
+      </Routes>
+    </Router>
   );
 }
 
-function App() {
-  const router = createHashRouter([
-    {
-      children: [
-        { element: <Home />, path: "/" },
-        { element: <Dockyard />, path: "/dockyard" },
-      ],
-      element: <Root />,
-    },
-  ]);
-
-  return <RouterProvider router={router} />;
-}
-
 export default App;
+
+// import Home from "./views/Home";
+// import Dockyard from "./views/Dockyard";
+
+// import { createHashRouter, Outlet, RouterProvider } from "react-router-dom";
+
+// function Root() {
+//   return (
+//     <>
+//       <main>
+//         <Outlet />
+//       </main>
+//     </>
+//   );
+// }
+
+// function App() {
+//   const router = createHashRouter([
+//     {
+//       children: [
+//         { element: <Home />, path: "/" },
+//         { element: <Dockyard />, path: "/dockyard" },
+//       ],
+//       element: <Root />,
+//     },
+//   ]);
+
+//   return <RouterProvider router={router} />;
+// }
+
+// export default App;
